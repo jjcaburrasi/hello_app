1b99b869c196b45414f8b77bdd54e03721e3e7d9
Jjcaburrasi <jjcaburrasi@gmail.com>
2022-07-22 16:24:44 +0200

    probando

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
     def hello
-        render html: "HOLA MAMI ESTOY EN HEROKU!!"
+        render html: "HOLA MAMI ESTOY EN HEROKU!! Y NO SE QUE PASA QUE SE BORRA CUANDO QUIERE!"
     end
 end
